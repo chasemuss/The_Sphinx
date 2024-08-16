c213e9c9305d21094005a917c891671b5f32ad4d
chasemuss <chasemuss@gmail.com>
2024-08-15 21:33:18 -0500

    Cleaned up commands

--- build_the_sphinx.cmd
+++ build_the_sphinx.cmd
@@ -1,4 +1,3 @@
 docker build -t the_sphinx .
-docker tag the_sphinx chasemuss/the_sphinx
-docker push chasemuss/the_sphinx
-docker run -d chasemuss/the_sphinx
+docker tag the_sphinx the_sphinx
+docker run -d the_sphinx
