d179d28430fb43c9e804aacd37f70616426b04c6
chasemuss <chasemuss@gmail.com>
2025-04-08 11:30:37 -0500

    General Update before Deployment

--- aws_deployment.cmd
+++ aws_deployment.cmd
@@ -1,3 +1,7 @@
+git add .
+git commit -m "General Update before Deployment"
+git push
+
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 800723880739.dkr.ecr.us-east-2.amazonaws.com
 docker build -t the_sphinx .
 docker tag the_sphinx:latest 800723880739.dkr.ecr.us-east-2.amazonaws.com/the_sphinx:latest
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-ecr_repository_url = "800723880739.dkr.ecr.us-east-2.amazonaws.com/the_sphinx:latest"
-discord_bot_token  = "MTIwMjYyNjA5MzI3NDc2NzQwMA.GeTptj.LMOKMHzzWIlAL2-8hQYe1F69VBDCG1vSqwDnHg"
